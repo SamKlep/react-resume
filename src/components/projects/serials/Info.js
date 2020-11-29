3b61f6736749984dd27952f3a1a6fefd481ce04d
SamKlep <shamdemann23@gmail.com>
2020-11-29 17:10:10 -0600

    Serials content.

--- src/components/projects/serials/Info.js
+++ src/components/projects/serials/Info.js
@@ -7,47 +7,45 @@ const Info = () => {
     <div>
       <Container>
         <Row className='justify-content-lg-center mb-5'>
-          <Col lg={6}>
+          <Col>
             <h1>Serial Killers & Unsolved Mysteries</h1>
             <p className='lead'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a
-              viverra ex, in consequat justo. Etiam volutpat in magna sed
-              lobortis. Etiam volutpat, ligula vitae interdum placerat, dolor
-              turpis varius dolor, ac imperdiet lacus nisi sed felis.
+              There are more than 222,000 unsolved murders since 1980 and over
+              2,000 current active serial killers as of July 2020. This type of
+              phenomenon cannot be ignored. Here you will find the organized
+              information in regards to Serial Killers and Unsolved Mysteries.
             </p>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a
-              viverra ex, in consequat justo. Etiam volutpat in magna sed
-              lobortis. Etiam volutpat, ligula vitae interdum placerat, dolor
-              turpis varius dolor, ac imperdiet lacus nisi sed felis.
+              Browse the top 20 serial killers of all time, get to the bottom of
+              the psychological intentions of the killer; then when you’ve had
+              enough gruesome details— switch over to the top unsolved mysteries
+              of the world! There’s enough content to make you walk swiftly
+              through the dark.
             </p>
           </Col>
         </Row>
         <hr />
-        <Row className='mt-5 mb-5'>
-          <Col>
+        <Row className='mt-5 mb-5 align-items-center'>
+          <Col className='justify-content-lg-center'>
             <h1>Tech Stack</h1>
             <p className='lead'>
               {' '}
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a
-              viverra ex, in consequat justo. Etiam volutpat in magna sed
-              lobortis. Etiam volutpat, ligula vitae interdum placerat, dolor
-              turpis varius dolor, ac imperdiet lacus nisi sed felis.
+              This project was meant to be a full-stack exercise in organizing
+              content and layout experimentation. I used Node, Express, and
+              Mongo for the backend and data persistence; React, CSS & Bootstrap
+              on the frontend.
             </p>
           </Col>
-          <Col>
-            <ListGroup>
-              <ListGroup.Item className='bg-light'>
-                Cras justo odio
-              </ListGroup.Item>
-              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-              <ListGroup.Item className='bg-light'>
-                Morbi leo risus
-              </ListGroup.Item>
-              <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-              <ListGroup.Item className='bg-light'>
-                Vestibulum at eros
-              </ListGroup.Item>
+          <Col className=' text-center'>
+            <ListGroup className='mx-auto skills-list'>
+              <ListGroup.Item className='bg-light'>Node</ListGroup.Item>
+              <ListGroup.Item>Express</ListGroup.Item>
+              <ListGroup.Item className='bg-light'>React</ListGroup.Item>
+              <ListGroup.Item>Mongo</ListGroup.Item>
+              <ListGroup.Item className='bg-light'>CSS</ListGroup.Item>
+              <ListGroup.Item className='bg-light'>Bootstrap</ListGroup.Item>
+              <ListGroup.Item className='bg-light'>Git</ListGroup.Item>
+              <ListGroup.Item className='bg-light'>Heroku</ListGroup.Item>
             </ListGroup>
           </Col>
         </Row>
