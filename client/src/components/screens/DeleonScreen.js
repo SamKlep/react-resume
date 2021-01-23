e0ef701bf8acc8091a78b6d2046fcda1aa68d8ca
SamKlep <shamdemann23@gmail.com>
2021-01-23 16:22:10 -0600

    Deleted unused photos.

--- client/src/components/screens/DeleonScreen.js
+++ client/src/components/screens/DeleonScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Main from '../projects/deleon/Main'
 import Content from '../projects/deleon/Content'
-import HousePhoto from '../projects/deleon/HousePhoto'
+import DeleonPhoto from '../projects/deleon/DeleonPhoto'
 import Info from '../projects/deleon/Info'
 import Videos from '../projects/deleon/Videos'
 
@@ -10,7 +10,7 @@ const DeleonScreen = () => {
     <div>
       <Main />
       <Content />
-      <HousePhoto />
+      <DeleonPhoto />
       <Videos />
       <Info />
     </div>
