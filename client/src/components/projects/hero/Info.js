75c28542753405effc039008dbbf2a3f9e70f379
byronklep <byronklep@gmail.com>
2021-06-01 11:20:18 -0500

    Heros styling -- list group.

--- client/src/components/projects/hero/Info.js
+++ client/src/components/projects/hero/Info.js
@@ -5,16 +5,16 @@ import { Link } from 'react-router-dom'
 const Info = () => {
   return (
     <div>
-      <Container className='info-container'>
-        <Row className='justify-content-lg-center mb-5'>
+      <Container className="info-container">
+        <Row className="justify-content-lg-center mb-5">
           <Col lg={8}>
             <h1>Hero's Corner</h1>
-            <p className='lead'>
+            <p className="lead">
               Anyone who has ever been an avid collector or reader of comic
               books will know how difficult it can be to obtain certain key
               issues at your local comic book shop.
             </p>
-            <p className='lead'>
+            <p className="lead">
               With that being said, create an account and log in to the{' '}
               <strong>Hero's Corner!</strong> See the condition and ratings of
               the books in question, read their summary, find out about key
@@ -24,10 +24,10 @@ const Info = () => {
           </Col>
         </Row>
         <hr />
-        <Row className='mt-5 mb-5 align-items-center '>
-          <Col className='stack-container'>
-            <h1 className='mt-5'>Tech Stack</h1>
-            <p className='lead'>
+        <Row className="mt-5 mb-5 align-items-center ">
+          <Col className="stack-container">
+            <h1 className="mt-5">Tech Stack</h1>
+            <p className="lead">
               {' '}
               This project is a full-stack e-commerce store built with React,
               Node, Bootstrap and utilizes Redux for state management. The user
@@ -37,19 +37,19 @@ const Info = () => {
             </p>
           </Col>
           <Col>
-            <ListGroup className='skills-list text-center mx-auto'>
-              <ListGroup.Item className='bg-light'>HTML</ListGroup.Item>
+            <ListGroup className="skills-list text-center mx-auto">
+              <ListGroup.Item className="bg-light">HTML</ListGroup.Item>
               <ListGroup.Item>CSS</ListGroup.Item>
-              <ListGroup.Item className='bg-light'>React</ListGroup.Item>
+              <ListGroup.Item className="bg-light">React</ListGroup.Item>
               <ListGroup.Item>React-Bootstrap</ListGroup.Item>
-              <ListGroup.Item className='bg-light'>Redux</ListGroup.Item>
-              <ListGroup.Item className='bg-light'>Git</ListGroup.Item>
-              <ListGroup.Item>Heroku</ListGroup.Item>
+              <ListGroup.Item className="bg-light">Redux</ListGroup.Item>
+              <ListGroup.Item>Git</ListGroup.Item>
+              <ListGroup.Item className="bg-light">Heroku</ListGroup.Item>
             </ListGroup>
           </Col>
         </Row>
-        <Link to='/'>
-          <Button className='mb-5'>Go Back</Button>
+        <Link to="/">
+          <Button className="mb-5">Go Back</Button>
         </Link>
       </Container>
     </div>
